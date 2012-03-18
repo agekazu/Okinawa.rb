ccd01ec7cf208bc50d0efc6c146c3ce364746046
rice-american <kazuto.agena@gmail.com>
2012-03-18 11:51:22 +0900

    インデントを揃えました

--- vending-machine.rb
+++ vending-machine.rb
@@ -18,13 +18,13 @@ class Vending_machine
       puts 'お金を入力(一回の入力で1000円以下、1円と5円は入らないよ!)'
       @money = gets.chomp
       @money = @money.to_i
-    
+
       if  @money < 0
         puts '-----やめてください。お金をとらないでください。'
         puts ''
         next
       end
-      
+
       #0入力
       if @money == 0
         puts '-----お願いします。お金をください。'
@@ -32,7 +32,7 @@ class Vending_machine
         next
       end
 
-     
+
       if @money % 10 == 0 && 0 < @money && @money <= 1000 
         puts '-----お金が' + @money.to_s + '円入りました'
         selecting
@@ -42,63 +42,63 @@ class Vending_machine
       end
     end #whileend 
   end 
-  
+
   def selecting 
     d1='1:おいしいみず(200円)'
     d2='2:サイコソーダ(300円)'
     d3='3:ミックスオレ(350円)'
 
-      puts ''
-      puts'商品を選んで下さい (1~3までの数字を入力)'
-      puts d1 
-      puts d2
-      puts d3
+    puts ''
+    puts'商品を選んで下さい (1~3までの数字を入力)'
+    puts d1 
+    puts d2
+    puts d3
 
-      drink = gets.chomp
-      drink = drink.to_i
+    drink = gets.chomp
+    drink = drink.to_i
 
-      case drink
-        when 1
-          @price = 200
-          d=d1
-        when 2
-          @price = 300 
-          d=d2
-        when 3
-          @price = 350
-          d=d3
-        else
-          puts '-----それはボタンではありませんやり直し'
-          selecting
-        end
- 
+    case drink
+    when 1
+      @price = 200
+      d=d1
+    when 2
+      @price = 300 
+      d=d2
+    when 3
+      @price = 350
+      d=d3
+    else
+      puts '-----それはボタンではありませんやり直し'
+      selecting
+    end
+
     puts '-----あなたは'+ d +'を選択しました。'
     puts ''
     oturi = @money-@price
-   
+
     if oturi < 0 
-        #Numeric#absメソッド=>絶対値を求める
-        puts '-----あ、お金が'+oturi.abs.to_s+'円、不足しています。再度入力してね'
-        puts ''
-        payment
+      #Numeric#absメソッド=>絶対値を求める
+      puts '-----あ、お金が'+oturi.abs.to_s+'円、不足しています。再度入力してね'
+      puts ''
+      payment
     else
-        puts 'ガタンッ'
-        puts '-----あなたはおもむろに' + d +'をとりだした。'
-        puts ''
-        change oturi
+      puts 'ガタンッ'
+      puts '-----あなたはおもむろに' + d +'をとりだした。'
+      puts ''
+      change oturi
     end
-   end
- 
+  end
+
   def change oturi
-        if oturi == 0
-          puts '-----お釣りはないのですよ。'
-        else 
-          puts 'お釣りです。チャリンチャリーン'
-          puts '-----あなたは' + oturi.to_s + '円のお釣りを受け取った'
-          puts ''
-        end
-     exit
-   end
+    if oturi == 0
+      puts '-----お釣りはないのですよ。'
+    else 
+      puts 'お釣りです。チャリンチャリーン'
+      puts '-----あなたは' + oturi.to_s + '円のお釣りを受け取った'
+      puts ''
+    end
+    exit
+  end
 
 end #classend
 
